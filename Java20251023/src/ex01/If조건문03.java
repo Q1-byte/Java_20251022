dfff65af2ec56df9f00abbfbcc39e71e307a0a4f
Q1-byte <gyuwon8440@gmail.com>
2025-10-23 15:53:25 +0900

    java study

---
+++ Java20251023/src/ex01/If조건문03.java
@@ -0,0 +1,34 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class If조건문03 {
+
+	public static void main(String[] args) {
+		/*
+		 * number >= 90 ~A
+		 * number >= 80 ~B
+		 * number >= 70 ~C
+		 * number >= 60 ~D
+		 * number < 60 ~F
+		 */
+		
+		int number = 79;
+		
+		if(number >= 90) {
+			System.out.println("A학점");
+		}else if(number >=80) {
+			System.out.println("B학점");
+		}else if(number >=70) {
+			System.out.println("C학점");
+		}else if(number >=60) {
+			System.out.println("D학점");
+		}else {
+			System.out.println("F학점");
+		}
+		
+		System.out.println("------ 프로그램종료 ------");
+		
+	}
+
+}
