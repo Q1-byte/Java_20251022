cfdb7516e8a7d0cc166fae454524f1a835af1b1d
Q1-byte <gyuwon8440@gmail.com>
2025-10-27 17:12:38 +0900

    java study

--- Java20251027_문자열/src/ex03/LottoEx02.java
+++ Java20251027_문자열/src/ex03/LottoEx02.java
@@ -18,11 +18,13 @@ public class LottoEx02 {
 		for(int i=0; i<45; i++)
 			lotto[i] = i+1;
 		
+		
 		for(int i=0; i<100; i++) {
 			int index = (int)(Math.random()*45);
 			int tmp = lotto[i%6];
 			lotto[i%6] = lotto[index];
 			lotto[index] = tmp;
+		
 		}
 		
 		for(int i=0; i<6; i++)
